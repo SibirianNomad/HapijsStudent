c8210206e3797117aee154e968da2244385a78d2
redouane__ <reduane@crypton.studio>
2021-08-19 16:36:15 +0700

    fix(test): sample test

---
+++ test/index.test.ts
@@ -0,0 +1,11 @@
+import Code from '@hapi/code'
+import Lab from '@hapi/lab'
+
+const { describe, it } = exports.lab = Lab.script()
+const { expect } = Code
+
+describe('test', () => {
+  it('test', async () => {
+    expect(true)
+  })
+})
