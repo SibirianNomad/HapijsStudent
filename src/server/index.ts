0f947c9db79e6c8339a22352f5b82993dac5a027
redouane__ <reduane@crypton.studio>
2021-08-23 18:19:09 +0700

    feat(server): clean up dead code

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,7 +10,6 @@ import * as HapiPulse from 'hapi-pulse'
 import * as Qs from 'qs'
 import config from '../config/config'
 import { handleValidationError, responseHandler } from './utils'
-import { tokenValidate } from './utils/auth'
 import SwaggerOptions from '../config/swagger'
 import { pinoConfig } from '../config/pino'
 
@@ -40,7 +39,7 @@ export const createServer = async (): Promise<Hapi.Server> => {
       }
     }
   })
-  server.realm.modifiers.route.prefix = '/api'
+
   // Регистрируем расширения
   await server.register([
     Basic,
@@ -51,44 +50,6 @@ export const createServer = async (): Promise<Hapi.Server> => {
     { plugin: Pino, options: pinoConfig(false) },
     { plugin: HapiSwagger, options: SwaggerOptions }
   ])
-
-  // Авторизация через соцсети
-  // server.auth.strategy('google', 'bell', {
-  //   provider: 'Google',
-  //   clientId: process.env.auth_google_id,
-  //   clientSecret: process.env.auth_google_secret,
-  //   password: process.env.auth_google_secret,
-  //   isSecure: false
-  // });
-  // server.auth.strategy('fb', 'bell', {
-  //   provider: 'Facebook',
-  //   clientId: Number(process.env.auth_fb_id),
-  //   clientSecret: process.env.auth_fb_secret,
-  //   password: process.env.auth_fb_cookie_password,
-  //   isSecure: false
-  // });
-  // server.auth.strategy('vk', 'bell', {
-  //   provider: 'VK',
-  //   clientId: Number(process.env.auth_vk_id),
-  //   clientSecret: process.env.auth_vk_secret,
-  //   password: process.env.auth_vk_cookie_password,
-  //   isSecure: false
-  // });
-
-  // Авторизация стандартная (логин+пароль)
-  // server.auth.strategy('simple', 'basic', { validate: basicAuthHandler });
-
-  // JWT Auth
-  server.auth.strategy('jwt-access', 'bearer-access-token', {
-    validate: tokenValidate('access')
-  })
-  server.auth.strategy('jwt-refresh', 'bearer-access-token', {
-    validate: tokenValidate('refresh')
-  })
-  server.auth.default('jwt-access')
-
-  // Загружаем маршруты
-  // server.route([...v1, ...v2])
 
   // Error handler
   server.ext('onPreResponse', responseHandler)
