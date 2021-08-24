587bb3c1966dcf95c666182e13e61c4921253d18
Redouane Sobaihi <redouane.sobaihi@live.com>
2021-08-25 01:02:28 +0700

    feat(logger): refactor logger module

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,4 +1,5 @@
 import { Plugin, Server } from '@hapi/hapi'
+import * as Pino from 'hapi-pino'
 import * as pkg from '../../package.json'
 
 export type Options = {}
@@ -7,6 +8,16 @@ export const Logging: Plugin<Options> = {
   name: 'logger',
   version: pkg.version,
   register: (server: Server, options: Options) => {
-
+    server.register({
+      plugin: Pino,
+      options: {
+        prettyPrint: {
+          colorize: true,
+          crlf: true
+        },
+        redact: ['req.headers.authorization'],
+        timestamp: () => `${new Date(Date.now()).toLocaleString()}`
+      } as unknown
+    })
   }
 }
