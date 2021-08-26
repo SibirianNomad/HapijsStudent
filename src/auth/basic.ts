d3792997446253de1dc946e0bae8c36898661f85
redouane__ <reduane@crypton.studio>
2021-08-26 11:59:59 +0700

    feat(auth): implement basic credentials validation func

--- src/auth/basic.ts
+++ src/auth/basic.ts
@@ -1,4 +1,20 @@
+import { Request, ResponseToolkit } from '@hapi/hapi'
+import * as bcrypt from 'bcrypt'
 
-export const validate = (): void => {}
+type AuthResult = {
+  isValid: boolean
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  credentials: any
+}
 
-export const verifyCredentials = (): void => {}
+export const validate = async (request: Request, username: string, password: string, context: ResponseToolkit): Promise<AuthResult> => {
+  const hashedPassword = await bcrypt.hash(password, 1)
+
+  const user = await request.server.methods.getUser(username, hashedPassword)
+  if (user) {
+    const { id, email } = user
+    return { isValid: true, credentials: { id, email } }
+  }
+
+  return { isValid: false, credentials: null }
+}
