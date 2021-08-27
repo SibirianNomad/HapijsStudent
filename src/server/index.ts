949370e7103f03035d155871edeaabc8b2cb1322
redouane__ <reduane@crypton.studio>
2021-08-27 15:13:44 +0700

    feat(server): wire modules to server and clean up dead code

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,11 @@ import * as Inert from '@hapi/inert'
 import * as Vision from '@hapi/vision'
 import * as HapiPulse from 'hapi-pulse'
 import * as Qs from 'qs'
+import { Api } from '../api'
+import { Auth } from '../auth'
 import config from '../config/config'
+import { Database, DatabaseOptions } from '../database'
+import { Websocket } from '../websocket'
 import { responseFilter } from './responseFilter'
 
 export const createServer = async (): Promise<Hapi.Server> => {
@@ -37,12 +41,43 @@ export const createServer = async (): Promise<Hapi.Server> => {
   /* Resonse filter */
   server.ext('onPreResponse', responseFilter)
 
+  await server.register<DatabaseOptions>({
+    plugin: Database,
+    options: null /* {
+      dialect: 'postgres',
+      host: '',
+      database: '',
+      username: '',
+      password: '',
+      port: 5432
+    } */
+  })
+
   await server.register({
     plugin: HapiPulse,
     options: {
       timeout: 15000,
       signals: ['SIGINT']
     }
+  })
+
+  await server.register({
+    plugin: Api,
+    options: {}
+  })
+
+  await server.register({
+    plugin: Auth,
+    options: {}
+  })
+
+  await server.register({
+    plugin: Websocket,
+    options: {}
+    /* Write unit tests to test websockets or
+     * use https://mdenushev.github.io/nes-cli/
+     * to test ws connections
+     */
   })
 
   return server
