e8fce216217cf20dc99b96b05c687722bfaebaa1
redouane__ <reduane@crypton.studio>
2021-08-27 18:25:24 +0700

    fix(auth): delete env vars

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -24,25 +24,25 @@ export const Auth: Plugin<AuthOptions> = {
     /* Авторизация через соцсети */
     server.auth.strategy('google', 'bell', {
       provider: 'Google',
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      password: process.env.AUTH_GOOGLE_SECRET,
+      clientId: '',
+      clientSecret: '',
+      password: '',
       isSecure: false
     })
 
     server.auth.strategy('facebook', 'bell', {
       provider: 'Facebook',
-      clientId: Number(process.env.AUTH_FB_ID),
-      clientSecret: process.env.AUTH_FB_SECRET,
-      password: process.env.AUTH_FB_COOKIE_PASSWORD,
+      clientId: Number(0),
+      clientSecret: '',
+      password: '',
       isSecure: false
     })
 
     server.auth.strategy('vkontakt', 'bell', {
       provider: 'VK',
-      clientId: Number(process.env.AUTH_VK_ID),
-      clientSecret: process.env.AUTH_VK_SECRET,
-      password: process.env.AUTH_VK_COOKIE_PASSWORD,
+      clientId: Number(0),
+      clientSecret: '',
+      password: '',
       isSecure: false
     })
   }
