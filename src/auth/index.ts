575836509c5b753ce111301f3ec3a8eab7f09e12
redouane__ <reduane@crypton.studio>
2021-08-27 18:49:57 +0700

    fix(auth): delete cookie auth and thirdparty auth

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,6 +2,8 @@ import { Plugin, Server } from '@hapi/hapi'
 import * as basic from './basic'
 import * as bearer from './jwt'
 import * as pkg from '../../package.json'
+import * as HapiBearer from 'hapi-auth-bearer-token'
+import * as HapiBasic from '@hapi/basic'
 
 export type AuthOptions = {
   // TODO: add additional options that needs to be passed to this module
@@ -11,39 +13,17 @@ export const Auth: Plugin<AuthOptions> = {
   name: 'auth',
   version: pkg.version,
   register: async (server: Server, options: AuthOptions) => {
+    await server.register([
+      HapiBearer,
+      HapiBasic
+    ])
+
     server.auth.strategy('bearer', 'bearer-access-token', {
       validate: bearer.validate
     })
 
     server.auth.strategy('basic', 'basic', {
       validate: basic.validate
-    })
-
-    server.auth.strategy('session', 'cookie')
-
-    /* Авторизация через соцсети */
-    server.auth.strategy('google', 'bell', {
-      provider: 'Google',
-      clientId: '',
-      clientSecret: '',
-      password: '',
-      isSecure: false
-    })
-
-    server.auth.strategy('facebook', 'bell', {
-      provider: 'Facebook',
-      clientId: Number(0),
-      clientSecret: '',
-      password: '',
-      isSecure: false
-    })
-
-    server.auth.strategy('vkontakt', 'bell', {
-      provider: 'VK',
-      clientId: Number(0),
-      clientSecret: '',
-      password: '',
-      isSecure: false
     })
   }
 }
