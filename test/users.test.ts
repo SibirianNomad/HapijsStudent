4438e14608fe8cc6f017deefb1d60b6055c2bec6
redouane__ <reduane@crypton.studio>
2021-09-08 14:55:31 +0700

    feat(api): update tests

--- test/users.test.ts
+++ test/users.test.ts
@@ -3,6 +3,7 @@ import { Server } from '@hapi/hapi'
 import Lab from '@hapi/lab'
 import { Users } from '../src/api/users'
 import * as Mocks from './mocks'
+import { Auth } from '../src/auth'
 
 const { describe, it, suite, before } = exports.lab = Lab.script()
 const { expect } = Code
@@ -15,6 +16,16 @@ suite('Users API', () => {
       server.method('createUser', Mocks.createUser)
 
       server.method('isUniqueEmailAddress', Mocks.isUniqueEmailAddress)
+
+      await server.register({
+        plugin: Auth,
+        options: {
+          jwtRefreshTokenSecret: 'A86D1AC8-81C2-451A-9CEA-8665F4C55DE7',
+          jwtTokenSecret: '6FD3C2FF-D14C-4F8D-85EF-6662B065D125',
+          jwtRefreshTokenLifetime: 60,
+          jwtTokenLifetime: 30
+        }
+      })
 
       await server.register({
         plugin: Users
